94d76e6d9b2b3eb9856d2bcd92e4af841ae3fa4f
Dmytro Koval <dawidof@ukr.net>
2015-03-02 16:39:13 +0100

    added user

--- initial.sh
+++ initial.sh
@@ -3,6 +3,8 @@
 function init {
 	yum -y update
 	echo "System Updated"
+	install_basics
+	add_user
 
 }
 
@@ -12,4 +14,11 @@ function install_basics {
     yum -y install gcc gcc-c++ gettext-devel expat-devel curl-devel zlib-devel openssl-devel perl cpio 
     yum -y install pinfo
     echo "Installed Basics"
+}
+
+function add_user {
+	echo "User name:"
+	read username
+	adduser $username
+	passwd $username
 }
