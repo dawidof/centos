f2ef0350ee63b0f57136d59eba7d5072d2bba518
Dmytro Koval <dawidof@ukr.net>
2015-03-02 16:47:52 +0100

    sudoers

--- initial.sh
+++ initial.sh
@@ -5,6 +5,10 @@ function init {
 	echo "System Updated"
 	install_basics
 	add_user
+	change_ssh
+
+
+	install_psql
 
 }
 
@@ -21,4 +25,22 @@ function add_user {
 	read username
 	adduser $username
 	passwd $username
+	passwd
+	usermod -aG wheel $username
+
+	echo "%wheel        ALL=(ALL)       ALL" >> /etc/sudoers
+    echo "created wheel group"
+}
+
+function change_ssh {
+	echo "ClientAliveInterval 30\n" >> /etc/ssh/sshd_config
+	echo "TCPKeepAlive yes \n" >> /etc/ssh/sshd_config
+	echo "ClientAliveCountMax 99999\n" >> /etc/ssh/sshd_config
+	echo "Port 4500" >> /etc/ssh/sshd_config
+	service sshd restart
+}
+
+function install_psql {
+	yum install postgresql-libs -y
+	yum install postgresql-devel -y
 }
