b411105438b8b4cbee91a161853603121112bb5c
Dmytro Koval <dawidof@ukr.net>
2015-03-02 16:34:57 +0100

    up 1

--- initial.sh
+++ initial.sh
@@ -1,1 +1,15 @@
-initial.sh
+#!/bin/bash
+
+function init {
+	yum -y update
+	echo "System Updated"
+
+}
+
+
+function install_basics {
+    yum -y install wget curl rsync git sudo vim make which mlocate man vixie-cron readline-devel
+    yum -y install gcc gcc-c++ gettext-devel expat-devel curl-devel zlib-devel openssl-devel perl cpio 
+    yum -y install pinfo
+    echo "Installed Basics"
+}
